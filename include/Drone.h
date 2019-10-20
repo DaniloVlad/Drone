b14c9469129a3e7b4e136d2a53977501bc099103
Dat Huu Vo <dvo9@uwo.ca>
2019-10-20 18:03:58 -0400

    fix Drone.h

--- include/Drone.h
+++ include/Drone.h
@@ -6,25 +6,35 @@
 
 #include "Accelerometer.h"
 #include "Motor.h"
+#include "STATE.h"
 
-class Drone{
+class Drone
+{
 public:
-    Drone(int pin,int frequency);
+    Drone(int port, int freq, int m1_pin, int m2_pin, int m3_pin, int m4_pin, int acc_da_pin, int acc_clk_pin, int cc);
     ~Drone();
-    bool isRunning();
-    int start(int speed);
-    int stop();
-
-    /** Getters **/
-    int getPin();
-    int getFrequency();
-    int getDutyRange();
-
-    /** Setters **/
-    void setFrequency(int frequency);
-    void setDutyRange();
+    int handleInstruction(char INS);
+    int //called by server after receiving instruction
+    STATE getState();
+    void checkAlt(); //called by server, polls accelerometer to maintain drones altitude if state is ROT_L/R, RESET, ACC_X/Y
+    int *getGyroData();
+    int[3] int *getAccData();
+    int[3] int rotateLeft();
+    int rotateRight();
+    int moveForward();
+    int moveBackward();
+    int moveLeft();
+    int moveRight();
+    int climb();
+    int lower();
+    int setAllMotors(int speed);
+    int setMotorSpeed(int motor_id, int speed); //sets all motors to the same speed
+    int land();
 private:
     int Motors[4];
-    
+    Accelerometer acc;
+    STATE current_state;
+    int acc_data[3];
+    int gyro_data[3];
 };
 #endif
