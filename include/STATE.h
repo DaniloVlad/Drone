585d3ee323b31b44021ded5763299f281a3d01fa
danilovlad <danilo.vladicic@gmail.com>
2019-10-20 23:06:40 -0400

    Created STATE enum

--- include/STATE.h
+++ include/STATE.h
@@ -1,1 +1,15 @@
 //nothing right now
+
+typedef enum _state {
+    OFF,
+    INIT,
+    RESET,
+    ROT_L,
+    ROT_R,
+    ACC_F,
+    ACC_B,
+    ACC_L,
+    ACC_R,
+    RAISE,
+    LOWER
+} STATE;
