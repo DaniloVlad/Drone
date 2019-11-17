ee1315f7fa1e495eb4b0677ebd932d0e755e374c
Philip Radojcic <pradojci@uwo.ca>
2019-11-17 18:18:34 -0500

    Adding comments.

--- include/Client.h
+++ include/Client.h
@@ -5,19 +5,18 @@
 #define CLIENT_H
 #include "Socket.h"
 
+// Socket class that allows sending and receiving data to the client class.
 class Client : Socket
 {
-    
     public:
+      	// Specifies a base constructor, constructors with params and a deconstructor function.
         Client();
         Client(int port, char *address, int domain = AF_INET, int type = SOCK_STREAM, int protocol = 0);
         Client(int port, uint32_t address, int domain = AF_INET, int type = SOCK_STREAM, int protocol = 0);
-        ~Client() {
-            
-        }
+        ~Client();
+        
+        // Functions to send and recieve streams of data for coordination.
         int send(void *data, size_t length);
         int receive(void *buffer, size_t length);
-
-
 };
 #endif
