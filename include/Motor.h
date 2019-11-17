c9109a8c27b7e8128f4110ea57422e3936093f0d
Philip Radojcic <pradojci@uwo.ca>
2019-11-17 18:30:43 -0500

    Adding comments.

--- include/Motor.h
+++ include/Motor.h
@@ -7,33 +7,34 @@
 #include "Configuration.h"
 
 class Motor{
+  
+    // Defines all the variables that will be used in the construction of the motor object.
     private:
         int speed;
         int range;
         int frequency;
         unsigned int pin;
 
+    // Constructor function for the motor object.
     public:
+    	// Specifies a base constructor, constructor with partial and full params, and a deconstructor function.
         Motor(int pin);
-        Motor(int pin,int frequency);
+        Motor(int pin, int frequency);
         Motor(int pin, int frequency, int pwm_range);
         ~Motor();
 
-        /** Getters **/
-        int getPin() {
-        }
-        int getFrequency() {
-        }
-        int getDutyRange() {
-        }
-        int getSpeed() {
-        }
+        /** Getter Functions **/
+        int getPin();
+        int getFrequency();
+        int getDutyRange();
+        int getSpeed();
 
-        /** Setters **/
+        /** Setter Functions **/
         void setFrequency(int frequency);
         void setDutyRange(int range);
         int setSpeed(int speed);
 
+  		// Stop condition function.
         int stop();
         bool isRunning() {
             return (this -> speed != 0);
