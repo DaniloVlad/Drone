b20e2a8e6780d902ebadf5f86e0744a4b5b8b0d7
Philip Radojcic <pradojci@uwo.ca>
2019-11-17 18:39:46 -0500

    Adding comments.

--- Motor.cpp
+++ Motor.cpp
@@ -1,8 +1,10 @@
 #include "include/Motor.h"
 
+// Constructs and sets the base motor with only the pin parameter.
 Motor::Motor(int pin) {
     this -> pin = pin;
 
+    // If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(pin, PI_OUTPUT) != 0) {
         perror("Couldn't initialize pin as PI_OUTPUT");
         exit(EXIT_FAILURE);
@@ -13,9 +15,11 @@ Motor::Motor(int pin) {
 
 }
 
+// Constructs and sets the motor with only the pin and frequency paramaters.
 Motor::Motor(int pin, int frequency) {
     this -> pin = pin;  
 
+    // If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(pin, PI_OUTPUT) != 0) {
         perror("Couldn't initialize pin as PI_OUTPUT");
         exit(EXIT_FAILURE);
@@ -25,23 +29,26 @@ Motor::Motor(int pin, int frequency) {
     this -> setFrequency(frequency);
 }
 
+// Constructs and sets the motor with all specific paramaters input for it's components.
 Motor::Motor(int pin, int frequency, int pwm_range) {
     this -> pin = pin;
 
+    // If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(pin, PI_OUTPUT) != 0) {
         perror("Couldn't initialize pin as PI_OUTPUT");
         exit(EXIT_FAILURE);
     }
 
     this -> setDutyRange(pwm_range);
-
     this -> setFrequency(frequency);
 }
 
+// Deconstructor method for the motor.
 Motor::~Motor() {
     this -> stop();
 }
 
+// Setter function for the motor frequency, and sends an error message and exit's if the setter fails.
 void Motor::setFrequency(int frequency) {
     if((this -> frequency = gpioSetPWMfrequency(this -> pin, frequency)) == PI_BAD_USER_GPIO) {
         perror("Couldn't set PWM frequency");
@@ -49,6 +56,7 @@ void Motor::setFrequency(int frequency) {
     }
 }
 
+// Setter function for the motor range, and sends an error message and exit's if the setter fails.
 void Motor::setDutyRange(int range) {
     if((this -> range = gpioSetPWMrange(this -> pin, range)) == (PI_BAD_USER_GPIO | PI_BAD_DUTYRANGE)) {
         perror("Couldn't set PWM range");
@@ -56,22 +64,27 @@ void Motor::setDutyRange(int range) {
     }
 }
 
-//-1 on error, otherwise returns speed
+// Setter function for the motor speed.
+// On error returns -1, otherwise returns the speed.
 int Motor::setSpeed(int speed) {
+  	// If the speed is out of range, it returns -1.
     if(speed > this -> range || speed < 0) {
         perror("Speed out of range");
         return -1;
     }
+  	// Attempt to return the speed.
     else {
+      	// First check if the GPIO PWM can be set to 0 (off), send an error message and exit.
         if(gpioPWM(this -> pin, speed) != 0) {
             perror("Error setting speed");
             exit(EXIT_FAILURE);
         }
+      	// If everything works as intended, the speed is returned
         return (this -> speed = speed);
     }
 }
 
-//-1 on error, 0 on success
+// Stop condition function for the motor, which returns 0 when successful, and -1 on an error;
 int Motor::stop() {
     return (this -> setSpeed(0));
 }
