37da19630023e3cb63741bd9aa322b5edb20737f
Philip Radojcic <pradojci@uwo.ca>
2019-11-17 18:13:57 -0500

    Adding comments.

--- Accelerometer.cpp
+++ Accelerometer.cpp
@@ -1,93 +1,117 @@
 #include "include/Accelerometer.h"
 
+// Constructs the accelerometer without any specific paramater input for it's components.
 Accelerometer::Accelerometer() {
     this -> i2c_addr = I2C_ADDR_ACC;
     this -> i2c_bus = I2C_BUS;
     this -> data_pin = I2C_SDA;
     this -> clock_pin = I2C_SCL;
 
+  	// If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(this -> data_pin, PI_ALT0) != 0) {
         perror("Couldn't set mode");
         exit(EXIT_FAILURE);
 
     }
-
+  
+  	// If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(this -> clock_pin, PI_ALT0) != 0) {
         perror("Couldn't set mode");
         exit(EXIT_FAILURE);
 
     }
 
+  	// 
     if((this -> i2c_handle = i2cOpen(this -> i2c_bus, this -> i2c_addr, 0)) == (PI_BAD_I2C_BUS | PI_BAD_I2C_ADDR | PI_BAD_FLAGS | PI_NO_HANDLE | PI_I2C_OPEN_FAILED)) {
         perror("Couldn't initialize i2c device");
         exit(EXIT_FAILURE);
     }
 }
+
+// Constructs the accelerometer with specific paramater input for it's components.
 Accelerometer::Accelerometer(int i2c_addr, int i2c_bus,int data_pin, int clock_pin) {
+  
+    //
     if(i2c_bus != 0 || i2c_bus != 1) {
         perror("I2C bus must be either 0 or 1");
     }
 
+  	// Set components to given parameters.
     this -> i2c_addr = i2c_addr;
     this -> i2c_bus = i2c_bus;
     this -> data_pin = data_pin;
     this -> clock_pin = clock_pin;
 
-    //ALT0 pg 102 "BCM ARM PERIPHERALS"
+  	// If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(this -> data_pin, PI_ALT0) != 0) {
         perror("Couldn't set mode");
         exit(EXIT_FAILURE);
 
     }
-
+  
+  	// If the GPIO mode is unable to be set to alternative mode 0, send an error message and exit.
     if(gpioSetMode(this -> clock_pin, PI_ALT0) != 0) {
         perror("Couldn't set mode");
         exit(EXIT_FAILURE);
 
     }
 
+   	//
     if((this -> i2c_handle = i2cOpen(this -> i2c_bus, this -> i2c_addr, 0)) == (PI_BAD_I2C_BUS | PI_BAD_I2C_ADDR | PI_BAD_FLAGS | PI_NO_HANDLE | PI_I2C_OPEN_FAILED)) {
         perror("Couldn't initialize i2c device");
         exit(EXIT_FAILURE);
     }
 }
 
+// Deconstructor method for the accelerometer.
 Accelerometer::~Accelerometer() {
     i2cClose(this -> i2c_handle);
 }
 
+// Getter function for the gyroscope's current X-axis data.
 int Accelerometer::getGyroX() {
     return (this -> gyroX = i2cReadWordData(this -> i2c_handle, GYRO_X_H));
 }
 
+// Getter function for the gyroscope's current Y-axis data.
 int Accelerometer::getGyroY() {
     return (this -> gyroX = i2cReadWordData(this -> i2c_handle, GYRO_Y_H));
 }
 
+// Getter function for the gyroscope's current Z-axis data.
 int Accelerometer::getGyroZ() {
     return (this -> gyroX = i2cReadWordData(this -> i2c_handle, GYRO_Z_H));
 }
 
+// Getter function for the accelerometer's current X-axis data.
 int Accelerometer::getAccX() {
     //reads 16-bit word starting at register
     return (this -> accX = i2cReadWordData(this -> i2c_handle, ACC_X_H));
 }
 
+// Getter function for the accelerometer's current Y-axis data.
 int Accelerometer::getAccY() {
     //reads 16-bit word starting at register
     return (this -> accX = i2cReadWordData(this -> i2c_handle, ACC_Y_H));
 }
 
+// Getter function for the accelerometer's current Z-axis data.
 int Accelerometer::getAccZ() {
     //reads 16-bit word starting at register
     return (this -> accX = i2cReadWordData(this -> i2c_handle, ACC_Z_H));
 }
 
+// Getter function for the gyroscope's current X, Y and Z axis data.
 int *Accelerometer::getGyroXYZ() {
-
+	getGyroX();
+  	getGyroY();
+  	getGyroZ();
 }
 
+// Getter function for the accelerometer's current X, Y and Z axis data.
 int *Accelerometer::getAccXYZ() {
-
+	getAccX();
+  	getAccY();
+  	getAccZ();
 }
 
