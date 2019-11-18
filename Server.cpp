28dbc7d7beefc41348e5341809aac0433286f067
Philip Radojcic <pradojci@uwo.ca>
2019-11-17 19:15:25 -0500

    Adding comments.

--- Server.cpp
+++ Server.cpp
@@ -1,85 +1,102 @@
 #include "include/Server.h"
 
+// Constructs the server socket without any specific paramater input for it's components.
 Server::Server() : Socket() {
     this -> client_addr = (struct sockaddr_in *) malloc(sizeof(sockaddr_in));
     this -> client_socklen = sizeof(sockaddr_in);
 
+  	// 
     memset(this -> client_addr, 0, sizeof(sockaddr_in));
 
     int opt = 1;
     
+  	// Error check - attempting to set socket options. If failed, sends an error message and exit.
     if(setsockopt(this -> socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
         perror("Couldn't set socket options");
         exit(EXIT_FAILURE);
     }
-
+  
+  	// Error check - attempting to bind the socket. If failed, sends an error message and exit.
     if(bind(this -> socketfd, (sockaddr *) this -> addr, this -> socklen) < 0) {
         perror("Couldn't bind socket!");
         exit(EXIT_FAILURE);
     }
-
+  
+  	// Error check - attempting to listen to the socket. If failed, sends an error message and exit.
     if(listen(this -> socketfd, 1) < 0) {
         perror("Couldn't listen on socket");
         exit(EXIT_FAILURE);
     }
 }
 
+// Constructs the server socket with specific paramater input for it's components, using the char address.
 Server::Server(int port, char *address, int domain, int type, int protocol) : Socket(port, address, domain, type, protocol) {
     
     this -> client_addr = (struct sockaddr_in *) malloc(sizeof(sockaddr_in));
     this -> client_socklen = sizeof(sockaddr_in);
 
+  	//
     memset(this -> client_addr, 0, sizeof(sockaddr_in));
 
     int opt = 1;
 
+    // Error check - attempting to set socket options. If failed, sends an error message and exit.
     if(setsockopt(this -> socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
         perror("Couldn't set socket options");
         exit(EXIT_FAILURE);
     }
     
+  	// Error check - attempting to bind the socket. If failed, sends an error message and exit.  
     if(bind(this -> socketfd, (sockaddr *) this -> addr, this -> socklen) < 0) {
         perror("Couldn't bind socket!");
         exit(EXIT_FAILURE);
     }
-
+  
+  	// Error check - attempting to listen to the socket. If failed, sends an error message and exit.
     if(listen(this -> socketfd, 1) < 0) {
         perror("Couldn't listen on socket");
         exit(EXIT_FAILURE);
     }
 }
 
+// Constructs the server socket with specific paramater input for it's components, using the uint32_t address.
 Server::Server(int port, uint32_t address, int domain, int type, int protocol) : Socket(port, address, domain, type, protocol) {
     
     this -> client_addr = (struct sockaddr_in *) malloc(sizeof(sockaddr_in));
     this -> client_socklen = sizeof(sockaddr_in);
 
+  	//
     memset(this -> client_addr, 0, sizeof(sockaddr_in));
 
     int opt = 1;
 
+    // Error check - attempting to set socket options. If failed, sends an error message and exit.
     if(setsockopt(this -> socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
         perror("Couldn't set socket options");
         exit(EXIT_FAILURE);
     }
     
+  	// Error check - attempting to bind the socket. If failed, sends an error message and exit.  
     if(bind(this -> socketfd, (sockaddr *) this -> addr, this -> socklen) < 0) {
         perror("Couldn't bind socket!");
         exit(EXIT_FAILURE);
     }
 
+    // Error check - attempting to listen to the socket. If failed, sends an error message and exit.
     if(listen(this -> socketfd, 1) < 0) {
         perror("Couldn't listen on socket");
         exit(EXIT_FAILURE);
     }
 }
 
+// Deconstructor method for the server socket.
 Server::~Server() {
     if(this -> client_addr) free(this -> client_addr);
     printf("Closing server socket...\n");
     close(new_socket);
 }
 
+// Starter function that attempts to start the server. If failed, sends an error message and exit.
 int Server::startServer() {
     if((this -> new_socket = accept(this -> socketfd, (sockaddr *) this -> client_addr, (socklen_t *) & this -> client_socklen)) < 0) {
         perror("Error accepting connection!");
@@ -88,10 +105,12 @@ int Server::startServer() {
     return 0;
 }
 
+// Returns the information read in to the server socket.
 int Server::send(void *data, size_t length) {
     return write(this -> new_socket, data, length);
 }
 
+// Returns the information to write in to the server socket.
 int Server::receive(void * buffer, size_t length) {
     return read(this -> new_socket, buffer, length);
 }
