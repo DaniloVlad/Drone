964d87255a3b09046f1fd933e9e8c4930e2de405
M. Hadi Badawi <mbadawi3@compute.gaul.csd.uwo.ca>
2019-11-17 22:29:44 -0500

    added function calls for Drone.cpp

--- Drone.cpp
+++ Drone.cpp
@@ -1,5 +1,98 @@
-#include "Drone.h"
+#include "include/Drone.h"
+#include "Motor.h"
+#include "STATE.h"
+#include "Server.h"
 
-int main(){
+Drone::Drone(int port, int freq, int m1_pin, int m2_pin, int m3_pin, 
+	int m4_pin, int acc_da_pin, int acc_clk_pin, int cc){
+	
+
 
 }
+
+Drone::~Drone(){
+
+}
+
+int Drone::handleInstruction(char INS){
+
+
+}
+
+
+STATE Drone::getState(){
+
+
+}
+
+void Drone::checkAlt(){
+
+
+
+}
+
+int Drone::*getGyroData(){
+
+
+}
+
+int Drone::*getAccData(){
+
+}
+
+int Drone::rotateLeft(){
+
+
+
+}
+
+int Drone::rotateRight(){
+
+
+}
+
+int Drone::moveForward(){
+
+
+}
+
+int Drone::moveBackward(){
+
+
+}
+
+int Drone::moveLeft(){
+
+
+}
+
+
+int Drone::moveRight(){
+
+
+}
+
+int Drone::climb(){
+
+
+}
+
+int Drone::lower(){
+
+
+}
+
+
+int Drone::setAllMotors(int speed){
+
+
+}
+
+
+int Drone::setMotorSpeed(int motor_id, int speed){
+
+
+}
+
+
+;
