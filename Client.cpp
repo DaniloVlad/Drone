7ce051551584a91404efd5ce3ddae091a0a3ab38
Philip Radojcic <pradojci@uwo.ca>
2019-11-24 20:53:26 -0500

    Added more comments, and cleaned code format.

--- Client.cpp
+++ Client.cpp
@@ -1,6 +1,9 @@
 #include "include/Client.h"
 
 // Constructs the client without any specific paramater input for it's components.
+/* Function: Client Constructor
+*   @breif - Constructs the client without any specific paramater input for it's components.
+*/  
 Client::Client() {
     Socket();
   	// Attempts to connect to the client socket after creation, and outputs an error message for exiting if the attempt fails.
@@ -11,6 +14,14 @@ Client::Client() {
 }
 
 // Constructs the client with specific paramater input for it's components, and a char address type.
+/* Function: Client Constructor
+*   @breif - Constructs the client with specific paramater input for it's components, and a char address type.
+*   @param port - port for the client.
+*   @param address - address for the client. 
+*   @param domain - domain for the client.
+*   @param type - type of data for the client.
+*   @param protocol - protocol the client is using.
+*/  
 Client::Client(int port, char *address, int domain, int type, int protocol) {
     Socket(port, address, domain, type, protocol);
     // Attempts to connect to the client socket after creation, and outputs an error message for exiting if the attempt fails.
@@ -21,6 +32,14 @@ Client::Client(int port, char *address, int domain, int type, int protocol) {
 }
 
 // Constructs the client with specific paramater input for it's components, and a uint32_t address type.
+/* Function: Client Constructor
+*   @breif - Constructs the client with specific paramater input for it's components, and a uint32_t address type.
+*   @param port - port for the client.
+*   @param address - address for the client. 
+*   @param domain - domain for the client.
+*   @param type - type of data for the client.
+*   @param protocol - protocol the client is using.
+*/  
 Client::Client(int port, uint32_t address, int domain, int type, int protocol) {
     Socket(port, address, domain, type, protocol);
     // Attempts to connect to the client socket after creation, and outputs an error message for exiting if the attempt fails.
@@ -31,11 +50,21 @@ Client::Client(int port, uint32_t address, int domain, int type, int protocol) {
 }
 
 // Returns the information read in to the client socket.
+/* Function: Recieve
+*   @breif - Returns the information read in to the client socket.
+*   @param data - the data to be recieved by the client.
+*   @param length - the size/length of the data.
+*/  
 int Client::receive(void * data, size_t length) {
     return read(this -> socketfd, data, length);
 }
 
 // Returns the information to write in to the client socket.
+/* Function: Send
+*   @breif - Returns the information to write in to the client socket.
+*   @param buffer - the data to be sent by the client. 
+*   @param length - the size/length of the data. 
+*/  
 int Client::send(void *buffer, size_t length) {
     return write(this -> socketfd, buffer, length);
 }
