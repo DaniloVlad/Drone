2ac9ce400b23055243acb34398a408d54ae64209
danilovlad <danilo.vladicic@gmail.com>
2019-11-28 15:07:38 -0500

    Finish main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include <pigpio.h>
 #include "include/Drone.h"
 
 int main() {
-    Drone *drone = new Drone();
-    drone -> startDrone();
+    if(gpioInitialise() < 0) {
+        std::cout << "Error starting pigpio: Run sudo main and if problem still presists run sudo killall pigpio" << std::endl;
+        return -1;
+    }
+    else {
+        std::cout << "Starting drone... " << std::endl;
+        Drone *drone = new Drone();
+        drone -> startDrone();
+
+    }
 }
